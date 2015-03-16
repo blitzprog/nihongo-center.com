c5af6606e0fd8bce29eafa79fa96c95d0dda3555
Eduard Urbach <e.urbach@gmail.com>
2015-03-16 12:00:11 +0900

    Added controller

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var aero = require("aero");
 
-aero.start("config.json");
+aero.start();
--- pages/login/login.js
+++ pages/login/login.js
@@ -1,1 +1,21 @@
-/* empty */
+"use strict";
+
+module.exports = {
+    get: function(data) {
+        console.log("GET:", data);
+        
+        return {
+            "email": data.email,
+            "password": data.password
+        };
+    },
+    
+    post: function(data) {
+        console.log("POST:", data);
+        
+        return {
+            "email": data.email,
+            "password": data.password
+        };
+    }
+};
