f2b64cd5cbca5517bb2185a07c0796d618a8f3ba
Eduard Urbach <e.urbach@gmail.com>
2015-04-08 11:31:16 +0900

    Started working on adding new family members

--- pages/profile/profile-save.js
+++ pages/profile/profile-save.js
@@ -1,5 +1,22 @@
+/*eslint-env browser, jquery */
+/*global aero*/
+
 var onContentLoaded = function() {
 	document.removeEventListener("DOMContentLoaded", onContentLoaded);
+	
+	var updateContent = function(response) {
+		var focusedElementId = $(document.activeElement).attr("id");
+		
+		aero.$content.html(response);
+		aero.fireContentLoadedEvent();
+		
+		// Re-focus previously selected element
+		if(typeof focusedElementId !== "undefined")
+			$("#" + focusedElementId).focus();
+		
+		// Remove from cache
+		delete aero.cache.profile;
+	};
 	
 	var save = function() {
 		var $this = $(this);
@@ -7,22 +24,13 @@ var onContentLoaded = function() {
 		var value = $this.val();
 		
 		$.post("/raw/profile", {
+			function: "saveProfileField",
 			key: key,
 			value: value
 		}, function(response) {
 			console.log("Saved " + key);
 			
-			var focusedElementId = $(document.activeElement).attr("id");
-			
-			aero.$content.html(response);
-			aero.fireContentLoadedEvent();
-			
-			// Re-focus previously selected element
-			if(typeof focusedElementId !== "undefined")
-				$("#" + focusedElementId).focus();
-			
-			// Remove from cache
-			delete aero.cache["profile"];
+			updateContent(response);
 		});
 	};
 	
@@ -30,6 +38,16 @@ var onContentLoaded = function() {
 	$(".date-input").blur(save);
 	$(".tel-input").change(save);
 	$("select").change(save);
+};
+
+var addFamilyMember = function() {
+	$.post("/raw/profile", {
+		function: "addFamilyMember"
+	}, function(response) {
+		console.log("Saved " + key);
+		
+		updateContent(response);
+	});
 };
 
 document.addEventListener("DOMContentLoaded", onContentLoaded);
