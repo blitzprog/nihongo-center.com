215ca5df2fb5495e7a031c1ad7bff2e7ce2aa936
Eduard Urbach <e.urbach@gmail.com>
2015-05-11 16:10:54 +0900

    Layout controller

--- modules/upload.js
+++ modules/upload.js
@@ -1,6 +1,9 @@
 "use strict";
 
-let multer = require("multer");
+let
+	multer = require("multer"),
+	path = require("path"),
+	fs = require("fs-extra");
 
 module.exports = function(aero) {
 	const kiloByte = 1024 * 1024;
@@ -8,6 +11,21 @@ module.exports = function(aero) {
 	
 	aero.app.use(multer({
 		dest: "./uploads/",
+		/*changeDest: function(dest, req, res) {
+			if(typeof req.user === "undefined") {
+				console.error("User is undefined on file upload");
+				return dest;
+			}
+			
+			let uploadPath = path.join(dest, req.user.email);
+			
+			fs.ensureDirSync(uploadPath, function(error) {
+				if(error)
+					throw error;
+			});
+			
+			return uploadPath;
+		},*/
 		limits: {
 			fileSize: 20 * megaByte,
 			files: 1,
