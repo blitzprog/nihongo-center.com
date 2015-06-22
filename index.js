14fd8ec2dadccf29a606fa25af710adcafef4608
Eduard Urbach <e.urbach@gmail.com>
2015-06-22 14:13:19 +0900

    Production environment

--- index.js
+++ index.js
@@ -2,21 +2,27 @@
 
 let aero = require("aero");
 let fs = require("fs");
+let os = require("os");
 let merge = require("object-assign");
 let login = require("./modules/login");
 let upload = require("./modules/upload");
 
 let apiKeys = JSON.parse(fs.readFileSync("api-keys.json"));
+let production = os.hostname() === "ncenter";
+let host = production ? "my.nihongo-center.com" : "localhost:3002";
+
+if(!production)
+	console.log("Development Version");
 
 // Google
 let googleConfig = merge({
-	callbackURL: "http://my.nihongo-center.com/auth/google/callback",
+	callbackURL: `http://${host}/auth/google/callback`,
 	userInfoURL: "https://www.googleapis.com/plus/v1/people/me"
 }, apiKeys.google);
 
 // Facebook
 let facebookConfig = merge({
-	callbackURL: "http://my.nihongo-center.com/auth/facebook/callback"
+	callbackURL: `http://${host}/auth/facebook/callback`
 }, apiKeys.facebook);
 
 // Init login
