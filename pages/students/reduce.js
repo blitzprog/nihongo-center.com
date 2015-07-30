c7c6109c09ed578ec28b12b3486a755625977c21
Eduard Urbach <e.urbach@gmail.com>
2015-07-30 11:21:56 +0900

    Student list sorting

--- pages/students/reduce.js
+++ pages/students/reduce.js
@@ -1,7 +1,11 @@
 // Currently not in use
 
 function(valueList) {
-	return valueList.filter(function(student) {
+	valueList = valueList.filter(function(student) {
 		return student !== null;
-	}); 
+	});
+	valueList.sort(function(a, b) {
+		return (a.applicationDate !== null) - (b.applicationDate !== null);
+	});
+	return valueList;
 }
