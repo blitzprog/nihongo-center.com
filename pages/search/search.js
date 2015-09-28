38dcf3526fc647fa1d342c7161338e7af6014f98
Eduard Urbach <e.urbach@gmail.com>
2015-09-28 11:37:44 +0900

    Set limit to 30

--- pages/search/search.js
+++ pages/search/search.js
@@ -105,8 +105,8 @@ module.exports = {
 				return registeredFactor + courseFactor * 2 + progressFactor * 4 + appliedFactor * 8;
 			});
 			
-			if(students.length > 20)
-				students.length = 20;
+			if(students.length > 30)
+				students.length = 30;
 
 			render({
 				user: user,
