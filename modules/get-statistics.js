b535eedf4e72f08282d8bf8e526a8c71a0536c1f
Eduard Urbach <e.urbach@gmail.com>
2015-10-30 15:51:57 +0900

    Added uploads' original name

--- modules/get-statistics.js
+++ modules/get-statistics.js
@@ -4,34 +4,40 @@ module.exports = function(students) {
 		male: 0,
 		female: 0
 	};
-	
+
 	students.forEach(function(student) {
 		// Country
 		if(student.country && student.country.length > 1) {
 			if(student.country === "America")
 				student.country = "United States";
-			
+
 			if(countries[student.country])
 				countries[student.country] += 1;
 			else
 				countries[student.country] = 1;
 		}
-		
+
 		// Gender
 		gender[student.gender] += 1;
 	});
-	
+
+	var totalApplicants = students.filter(function(student) {
+		return student.applicationDate !== null;
+	}).length;
+	var applicantsAccepted = students.filter(function(student) {
+		return student.stage !== "apply" && student.stage !== "rejected";
+	}).length;
+	var applicantsRejected = students.filter(function(student) {
+		return student.stage === "declined";
+	}).length;
+	var applicantsRemaining = statistics.totalApplicants - statistics.applicantsAccepted - statistics.applicantsRejected
+
 	return {
 		totalStudents: students.length,
-		totalApplicants: students.filter(function(student) {
-			return student.applicationDate !== null;
-		}).length,
-		applicantsAccepted: students.filter(function(student) {
-			return student.stage !== "apply" && student.stage !== "rejected";
-		}).length,
-		applicantsRejected: students.filter(function(student) {
-			return student.stage === "declined";
-		}).length,
+		totalApplicants: totalApplicants,
+		applicantsAccepted: applicantsAccepted,
+		applicantsRejected: applicantsRejected,
+		applicantsRemaining: applicantsRemaining,
 		countries: countries,
 		gender: gender
 	};
